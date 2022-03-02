5d1e0c841784303eaf07b8c3f83ab91f903b2c7a
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-03-02 14:16:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.75%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int fib(int N) {
-        if(N == 0)  return 0;
-        if(N == 1)  return 1;
-        return fib(N-1) + fib(N-2);
+       double phi = (sqrt(5) + 1) / 2;
+        return round(pow(phi, N) / sqrt(5));
     }
 };
