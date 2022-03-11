6da4d51153402f67d754cc05ea0a1f890bd7d504
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-03-12 03:04:45 +0530

    Time: 11 ms (51.92%), Space: 11.8 MB (55.94%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -8,47 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* dummy = new ListNode(0);
-            dummy->next = head;
-        ListNode* temp1 = head;
-        ListNode* temp2 = head;
-        ListNode* prev ;
-        
-        if(!k || !head || !head->next)return head;
-        int f=1;
-        while(temp2 && temp2->next) {
-            f++;
-            temp2 = temp2->next;
-            
+        if (head == nullptr || head->next == nullptr) {
+            return head;
         }
-        
-        k %= f;
-        if(!k)return head;
-       
-        k = f - k;
-        
-         cout<<f<<" "<<k;
-        int i=0;
-        
-        
-        while(temp1 && temp1->next){
-            
-            if(i==k)
-                break;
-            prev=temp1;
-            temp1=temp1->next;
-            i++;
+        auto iter = head;
+        auto len = 1;
+        while (iter->next != nullptr) {
+            iter = iter->next; ++len;
         }
-        prev->next=NULL;
-        dummy->next=temp1;
-        temp2->next = head;
-        
-        return dummy->next;
-        
-        
-        
+        // close the list (create a ring)
+        iter->next = head;
+        iter = head;
+        // find one element before the new head
+        for (int i = 0; i < len - (k % len) - 1; ++i) {
+            iter = iter->next;
+        }
+        head = iter->next;
+        // open the list (break the ring)
+        iter->next = nullptr;
+        return head;
     }
 };
