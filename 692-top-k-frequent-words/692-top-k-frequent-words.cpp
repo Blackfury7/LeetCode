a65dad1c892dbb3ff9e77f2a1b0f7b55b26403da
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-03-14 02:20:48 +0530

    Time: 16 ms (65.76%), Space: 12.5 MB (73.53%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,50 +1,56 @@
 class Solution {
 public:
-    // struct myComp
-    // {
-    //     constexpr bool operator()(
-    //         pair<int, int> const &a,
-    //         pair<int, int> const &b)
-    //         const noexcept
-    //     {
-    //         return a.second < b.second;//max heap
-    //     }
-    // };
+    struct myComp
+    {
+        constexpr bool operator()(
+            pair<int, string> const &a,
+            pair<int, string> const &b)
+            const noexcept
+        {
+            // return a.first < b.first; //max heap
+            if(a.first == b.first)
+                return ((a.second).compare(b.second) < 0);
+            else
+                return (a.first > b.first);
+        }
+    };
     
-    public: static bool myComp(pair<int,string> a, pair<int,string> b) {
-        if(a.first == b.first)
-            return ((a.second).compare(b.second) < 0);
-        else
-            return (a.first > b.first);
-    }
+    // public: static bool myComp(pair<int,string> a, pair<int,string> b) {
+    //     if(a.first == b.first)
+    //         return ((a.second).compare(b.second) < 0);
+    //     else
+    //         return (a.first > b.first);
+    // }
     
     
     vector<string> topKFrequent(vector<string>& words, int k) {
          unordered_map<string,int> m;
-        vector<pair<int,string>> v;
+        // vector<pair<int,string>> v;
         
         for(auto i:words)m[i]++;
-        for(auto i:m){v.push_back(make_pair(i.second,i.first));}
-        sort(v.begin(), v.end(), myComp);
+        // for(auto i:m){v.push_back(make_pair(i.second,i.first));}
+        // sort(v.begin(), v.end(), myComp);
 
         // int n = v.size();
          vector<string> v1;
-        for(int i=0;i<k;i++){
-            v1.push_back(v[i].second);
-        }
+        // for(int i=0;i<k;i++){
+        //     v1.push_back(v[i].second);
+        // }
        
 
         
-//         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-//         for(auto i:m){
-//             pq.push(make_pair(i.second,i.first));
-//             if(pq.size()>k)pq.pop();
-//         }
+        priority_queue<pair<int,string>, vector<pair<int,string>>, myComp> pq;
+        for(auto i:m){
+            pq.push(make_pair(i.second,i.first));
+            if(pq.size()>k)pq.pop();
+        }
         
-//         while(!pq.empty()){
-//             v1.push_back(pq.top().second);
-//             pq.pop();
-//         }
+        while(!pq.empty()){
+            v1.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        reverse(v1.begin(), v1.end());
         
         
         return v1;
