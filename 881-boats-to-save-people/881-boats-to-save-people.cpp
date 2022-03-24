b9484f982beb0e70c99bfde30e4f14da9fcfcd90
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-03-25 00:43:17 +0530

    Time: 150 ms (17.60%), Space: 41.8 MB (99.71%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -4,13 +4,12 @@ public:
         sort(people.begin(), people.end());
         int n=people.size();
         int f=0;
-        for(int i=0,j=n-1; i<=j&&i<n&&j>=0;){
+        for(int i=0,j=n-1;i<=j;){
             if(people[i]+people[j] <= limit){
-                i++;j--;f++;
+                i++;j--;
             }
-            else{
-                j--;f++;
-            }
+            else j--;
+            f++;
         }
         return f;
     }
