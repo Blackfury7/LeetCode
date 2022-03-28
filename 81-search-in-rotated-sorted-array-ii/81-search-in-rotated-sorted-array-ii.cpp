68a0a704920cd71b4f60e6f8d64d71596cdbfcc8
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-03-28 16:42:54 +0530

    Time: 5 ms (73.46%), Space: 14 MB (71.85%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i] == target)return 1;
+        }
+        return 0;
+    }
+};
