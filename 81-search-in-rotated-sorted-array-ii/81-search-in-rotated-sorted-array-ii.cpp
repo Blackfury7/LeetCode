6ff83c199399e1b9bf7856d7c9c7b3ba5b5c7ac5
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-03-30 05:08:49 +0530

    Time: 8 ms (59.75%), Space: 13.9 MB (72.50%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,10 +1,44 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i] == target)return 1;
+       int n = nums.size();
+        if (n == 0) return false;
+        int end = n - 1;
+        int start = 0;
+        
+        while(start <= end) {
+            int mid = start + (end - start) /2;
+            if(nums[mid] == target){
+                return true;
+            }
+            if(!isBinarySearchHelpful(nums, start, nums[mid])){
+                start++;
+                continue;
+            }
+            
+            bool pivotArray = existsInFirst(nums, start, nums[mid]);
+            
+            bool targetArray = existsInFirst(nums, start, target);
+            
+            if(pivotArray ^ targetArray) {
+                if(pivotArray) start = mid +1;
+                else end = mid - 1;
+            } else {
+                if(nums[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
         }
-        return 0;
+        return false;
+    }
+    
+    bool isBinarySearchHelpful(vector<int>& nums, int start, int element) {
+        return nums[start] != element;
+    }
+    
+    bool existsInFirst(vector<int>& nums, int start, int element) {
+        return nums[start] <= element;
     }
 };
