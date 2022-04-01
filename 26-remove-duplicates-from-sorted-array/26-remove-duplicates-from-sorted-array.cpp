19979397979000f77a4cdbfb43420ed688126a26
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-01 12:31:17 +0530

    Time: 26 ms (21.30%), Space: 18.4 MB (36.64%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -4,17 +4,11 @@ public:
         int n = nums.size();
         int f=0;
         auto start = nums.begin();
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n; i++)
             if(nums[i] == nums[i-1]){nums[i-1]=101;f++;}
-        }
         int j =0;
-        for(int i=n-1; i>=0; i--){
-            if(nums[i] == 101)
-            {
-                nums.erase(start+i);
-            }
-        }
-        cout<<endl;
+        for(int i=n-1; i>=0; i--)
+            if(nums[i] == 101) nums.erase(start+i);
         return n - f;
     }
 };
