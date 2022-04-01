5bac53534aaf7293c5b1dd903d9263d2f3625027
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-02 03:16:02 +0530

    Time: 5 ms (10.15%), Space: 6.3 MB (15.40%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int ar[n+2];
-        ar[0] = 1; ar[1] = 2;
-        for(int i=2;i<n;i++){
-            ar[i] = ar[i-1] +  ar[i-2];
-        }
+        int fun(int n, vector<int> &dp){
+         if(n<=1)return dp[n]=1;
+        if(dp[n] != -1) return dp[n];
         
-        return ar[n-1];
+        int one = fun(n-1, dp);
+        int two = fun(n-2,dp);
+        
+        return dp[n] = one + two;
+    }
+    int climbStairs(int n) {
+       vector<int> dp(n+1,-1);
+         fun(n,dp);
+        return dp[n];
     }
 };
