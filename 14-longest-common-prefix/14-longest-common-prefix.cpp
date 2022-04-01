d1b8d5d6c26d8e5454e1a225e34f6148eeb6f863
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-01 12:12:11 +0530

    Time: 8 ms (36.02%), Space: 9.2 MB (78.71%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // string tmp, tmp1=strs[0];
-        // int n = strs.size();
-        // for(int i =1;i<n; i++){
-        //     int j=0,z=0; tmp="";
-        //     while(j<tmp1.length() && z < strs[i].length()){
-        //         if(tmp1[j] != strs[i][z])break;
-        //         tmp += tmp1[j];
-        //         j++;z++;
-        //     }
-        //     if(tmp.length() < tmp1.length())tmp1 = tmp;
-        // }
-        // return tmp1;
-        
-        
-        
+        string tmp, tmp1=strs[0];
         int n = strs.size();
-        if(n==0) return "";
-        
-        string ans  = "";
-        sort(begin(strs), end(strs));
-        string a = strs[0];
-        string b = strs[n-1];
-        
-        for(int i=0; i<a.size() && i<b.size(); i++){
-            if(a[i]!=b[i])break;
-                ans = ans + a[i];
-            
+        for(int i =1;i<n; i++){
+            int j=0,z=0; tmp="";
+            while(j<tmp1.length() && z < strs[i].length()){
+                if(tmp1[j] != strs[i][z])break;
+                tmp += tmp1[j];
+                j++;z++;
+            }
+            if(tmp.length() < tmp1.length())tmp1 = tmp;
         }
+        return tmp1;
         
-        return ans;
+        
+        
+//         int n = strs.size();
+//         if(n==0) return "";
+        
+//         string ans  = "";
+//         sort(begin(strs), end(strs));
+//         string a = strs[0];
+//         string b = strs[n-1];
+        
+//         for(int i=0; i<a.size() && i<b.size(); i++){
+//             if(a[i]!=b[i])break;
+//                 ans = ans + a[i];
+            
+//         }
+        
+//         return ans;
     }
 };
