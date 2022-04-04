1cc32921d1907b9b09ecd0f8671407d37298dff0
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-05 01:47:24 +0530

    Time: 141 ms (49.06%), Space: 31 MB (28.19%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<long long> hashSet;
-        for(long long num:nums)
-            hashSet.insert(num);
         
-        int longestStreak = 0;
         
-        for(long long i: nums){
-            if(!hashSet.count(i-1)){
-                int currentStreak = 1;
-                long long currentNum = i;
-                while(hashSet.count(currentNum+1)){
-                    currentStreak++;
-                    currentNum++;
-                }
-                longestStreak = max(longestStreak, currentStreak);
-            }
+        
+    //**************************************************************************************************
+    //     unordered_set<long long> hashSet;
+    //     for(long long num:nums)
+    //         hashSet.insert(num);
+    //     int longestStreak = 0;
+    //     for(long long i: nums){
+    //         if(!hashSet.count(i-1)){
+    //             int currentStreak = 1;
+    //             long long currentNum = i;
+    //             while(hashSet.count(currentNum+1)){
+    //                 currentStreak++;
+    //                 currentNum++;
+    //             }
+    //             longestStreak = max(longestStreak, currentStreak);
+    //         }
+    //     }
+    //     return longestStreak;
+    // }
+    //**************************************************************************************************
+        unordered_map<int, int> m;
+        int r = 0;
+        for (int i : nums) {
+            if (m[i]) continue;
+            r = max(r, m[i] = m[i + m[i + 1]] = m[i - m[i - 1]] = m[i + 1] + m[i - 1] + 1);
         }
-        return longestStreak;
+        return r;
     }
 };
