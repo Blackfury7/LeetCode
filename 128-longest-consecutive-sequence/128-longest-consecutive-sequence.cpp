05c276a8b41008787a75cdd7b3face5089f9196a
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-05 01:54:52 +0530

    Time: 89 ms (70.03%), Space: 28.8 MB (83.49%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,7 +2,20 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
+    //**************************************************************************************************
         
+        unordered_set<int> hashSet(nums.begin(), nums.end());
+        int res = 0;
+        for(int i:nums){
+            if(hashSet.find(i) == hashSet.end()) continue;
+            hashSet.erase(i);
+            int prev = i - 1, next = i + 1;
+            while(hashSet.find(prev) != hashSet.end()) hashSet.erase(prev--);
+            while(hashSet.find(next) != hashSet.end()) hashSet.erase(next++);
+            res = max(res, next - prev - 1);
+        }
+        return res;
+    }
         
     //**************************************************************************************************
     //     unordered_set<long long> hashSet;
@@ -23,12 +36,14 @@ public:
     //     return longestStreak;
     // }
     //**************************************************************************************************
-        unordered_map<int, int> m;
-        int r = 0;
-        for (int i : nums) {
-            if (m[i]) continue;
-            r = max(r, m[i] = m[i + m[i + 1]] = m[i - m[i - 1]] = m[i + 1] + m[i - 1] + 1);
-        }
-        return r;
-    }
+        
+    //     unordered_map<int, int> m;
+    //     int r = 0;
+    //     for (int i : nums) {
+    //         if (m[i]) continue;
+    //         r = max(r, m[i] = m[i + m[i + 1]] = m[i - m[i - 1]] = m[i + 1] + m[i - 1] + 1);
+    //     }
+    //     return r;
+    // }
+    //**************************************************************************************************
 };
