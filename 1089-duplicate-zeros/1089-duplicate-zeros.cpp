2058aa0ae10f0d7728c1969de2c0367cd0c7ab9e
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-06 05:57:10 +0530

    Time: 15 ms (51.10%), Space: 9.5 MB (94.21%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -13,7 +13,14 @@ public:
         
         
         //*****************************************************************************
-        //
+        //Though this C++ code is not intuitive, but the method is pretty cool.
+
+        // 1. count 0s, create a fake array, len + len(0s)
+        // 2. two pointer, i starts from len-1, j starts from len+len(0s)-1, 
+         // why? we need to avoid value overwrite, we have to loop reversely.
+        // 3. assign i value to j, when i is 0, assign 0 twice
+        // 4. add a condition, only perform the copy when j is under the actual array
+        
         int n = arr.size(), j = n + count(arr.begin(), arr.end(), 0);
         for (int i = n - 1; i >= 0; --i) {
             if (--j < n)
