e7bdd4830686a4278706183665280b4ab43dc7c9
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-07 01:56:16 +0530

    Time: 401 ms (47.08%), Space: 10.4 MB (74.57%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        unordered_map<int,int>m;
-        int res = 0, mod = 1e9 + 7;
-        for (int i = 0; i<arr.size(); i++) {
-            res = (res + m[target - arr[i]]) % mod;
-            for(int j=0; j<i; j++) {
-                m[arr[i] + arr[j]]++;
+        
+     //************************************************************************************************************   
+        int n = size(arr), ans = 0, mod = 1000000007;
+        sort(begin(arr), end(arr));
+        for(int i = 0; i < n - 2; i++){
+            int T = target - arr[i],  j = i + 1, k = n - 1;
+            while(j < k){
+                if(arr[j] + arr[k] == T){
+                    if(arr[j] == arr[k]){
+                        ans = (ans + (((k - j) * (k - j + 1)) >> 1) % mod) % mod;  break;
+                    } 
+                    else{
+                        int l = 1, r = 1;
+                        while(j + 1 < k && arr[j] == arr[j + 1]) j++, l++;
+                        while(k - 1 >= j && arr[k] == arr[k - 1]) k--, r++;
+                        ans += (l * r) % mod;
+                        j++, k--;
+                    }                    
+                }
+                else if(arr[j] + arr[k] < T) j++;
+                else k--;
             }
         }
-        return res;
+        return ans;
+        
+     //************************************************************************************************************   
+        // unordered_map<int,int>m;
+        // int res = 0, mod = 1e9 + 7;
+        // for (int i = 0; i<arr.size(); i++) {
+        //     res = (res + m[target - arr[i]]) % mod;
+        //     for(int j=0; j<i; j++) {
+        //         m[arr[i] + arr[j]]++;
+        //     }
+        // }
+        // return res;
+     //************************************************************************************************************   
     }
 };
