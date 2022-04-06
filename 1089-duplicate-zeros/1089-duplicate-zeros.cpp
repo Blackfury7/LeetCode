ae6524eb9bd11477e04a9ab7b2694dfd88468059
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-06 05:42:39 +0530

    Time: 31 ms (43.92%), Space: 9.7 MB (44.76%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size();
         for(int i = 0; i<n; i++){
             if(arr[i] == 0) {
-                arr.insert(arr.begin() + i++, 0); 
+                arr.insert(arr.begin() + ++i, 0); 
                 arr.pop_back();
             }
         }
