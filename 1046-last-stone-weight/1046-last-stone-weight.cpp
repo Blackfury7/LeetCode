3e39e352f424dfdebfab0236d46c493739d937fa
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-07 23:33:46 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -15,39 +15,50 @@ public:
         // }
         // return pq.empty() ? 0 : pq.top();
         
-        //******************************************************************************************
-        
-//         priority_queue<int> pq;
-//         for(auto i:stones)pq.push(i);
-        
-//         while(!pq.empty()){
-//             int a = pq.top();pq.pop();
-//             if(pq.empty())return a;
-//             int b = pq.top();pq.pop();
-//             a = a-b;
-//             if(pq.empty())return a;
-//             if(a)pq.push(a);
-//         }
-//         return 0;
         
         //******************************************************************************************
+        // Naive approach
+        // time complexity : O(N * NlogN)
+        // space complexity : O(1)
+        
+        // sort(stones.begin(),stones.end());
+        // int a=stones.size()-1;
+        // if(a==0){
+        //     return stones[a];
+        // }
+        // for(int i=0;i<stones.size();i++){
+        //     sort(stones.begin(),stones.end());
+        //     if(stones[a]>0 && stones[a-1]>0){
+        //         stones[a]=stones[a]-stones[a-1];
+        //         stones[a-1]=0;
+        //     }
+        //     else{
+        //         break;
+        //     }
+        // }
+        // return stones[a];
+        //******************************************************************************************
+        
+        // Recursion solution
+        
+        if(stones.size() == 0)
+            return 0;
+        if(stones.size() == 1)
+            return stones.front();
         
         sort(stones.begin(),stones.end());
-        int a=stones.size()-1;
-        if(a==0){
-            return stones[a];
-        }
-        for(int i=0;i<stones.size();i++){
-            sort(stones.begin(),stones.end());
-            if(stones[a]>0 && stones[a-1]>0){
-                stones[a]=stones[a]-stones[a-1];
-                stones[a-1]=0;
-            }
-            else{
-                break;
-            }
-        }
-        return stones[a];
+        
+        int a = stones[stones.size()-1];
+        int b = stones[stones.size()-2];
+        
+        stones.pop_back();
+        stones.pop_back();
+        
+        if(a == b)
+            return  lastStoneWeight(stones);
+        stones.push_back(a-b);
+        return lastStoneWeight(stones);
+    
         //******************************************************************************************
     }
 };
