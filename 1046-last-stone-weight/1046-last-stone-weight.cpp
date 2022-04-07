c18bfa1ea3f0a3e6fed4a1f8a1db78fb5e7e9242
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-07 23:23:48 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (97.59%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq;
-        for(auto i:stones)pq.push(i);
         
-        while(!pq.empty()){
-            int a = pq.top();pq.pop();
-            if(pq.empty())return a;
-            int b = pq.top();pq.pop();
-            a = a-b;
-            if(pq.empty())return a;
-            if(a)pq.push(a);
+        //Complexity
+        // Time O(NlogN)
+        // Space O(N)
+        
+        priority_queue<int> pq(begin(stones), end(stones));
+        while (pq.size() > 1) {
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            if (x > y) pq.push(x - y);
         }
-        return 0;
+        return pq.empty() ? 0 : pq.top();
+        
+        //**************************************************************
+        
+//         priority_queue<int> pq;
+//         for(auto i:stones)pq.push(i);
+        
+//         while(!pq.empty()){
+//             int a = pq.top();pq.pop();
+//             if(pq.empty())return a;
+//             int b = pq.top();pq.pop();
+//             a = a-b;
+//             if(pq.empty())return a;
+//             if(a)pq.push(a);
+//         }
+//         return 0;
     }
 };
