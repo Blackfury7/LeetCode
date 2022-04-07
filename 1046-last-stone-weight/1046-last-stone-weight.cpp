9c6d2024d49b85c55ab9c71bbe7d13d592adc884
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-07 23:27:36 +0530

    Time: 4 ms (37.54%), Space: 7.6 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         
+        //******************************************************************************************
         //Complexity
         // Time O(NlogN)
         // Space O(N)
         
-        priority_queue<int> pq(begin(stones), end(stones));
-        while (pq.size() > 1) {
-            int x = pq.top(); pq.pop();
-            int y = pq.top(); pq.pop();
-            if (x > y) pq.push(x - y);
-        }
-        return pq.empty() ? 0 : pq.top();
+        // priority_queue<int> pq(begin(stones), end(stones));
+        // while (pq.size() > 1) {
+        //     int x = pq.top(); pq.pop();
+        //     int y = pq.top(); pq.pop();
+        //     if (x > y) pq.push(x - y);
+        // }
+        // return pq.empty() ? 0 : pq.top();
         
-        //**************************************************************
+        //******************************************************************************************
         
 //         priority_queue<int> pq;
 //         for(auto i:stones)pq.push(i);
@@ -28,5 +29,25 @@ public:
 //             if(a)pq.push(a);
 //         }
 //         return 0;
+        
+        //******************************************************************************************
+        
+        sort(stones.begin(),stones.end());
+        int a=stones.size()-1;
+        if(a==0){
+            return stones[a];
+        }
+        for(int i=0;i<stones.size();i++){
+            sort(stones.begin(),stones.end());
+            if(stones[a]>0 && stones[a-1]>0){
+                stones[a]=stones[a]-stones[a-1];
+                stones[a-1]=0;
+            }
+            else{
+                break;
+            }
+        }
+        return stones[a];
+        //******************************************************************************************
     }
 };
