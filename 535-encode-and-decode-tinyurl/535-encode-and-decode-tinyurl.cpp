d6e81e819e11fedcb4f78e3d9aad8793652226c0
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-04-23 23:31:35 +0530

    Time: 10 ms (17.07%), Space: 6.9 MB (66.28%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,42 +1,57 @@
 class Solution {
-public:
+    public:
+        map<string,string> mp;
+        int n = 0;
+        // Encodes a URL to a shortened URL.
+        string encode(string longUrl) {
+            string res = "http://tinyurl.com/" + to_string(n);
+            n++;
+            mp[res] = longUrl;
+            return res;
+        }
 
-    unordered_map<string, string>mp;
-    int length = 6;
+        // Decodes a shortened URL to its original URL.
+        string decode(string shortUrl) {
+            return mp[shortUrl];
+        }
+    //***************************************************************************************
+
+//     unordered_map<string, string>mp;
+//     int length = 6;
     
-    string hashing(string longUrl){
-        string hashCode = "";
+//     string hashing(string longUrl){
+//         string hashCode = "";
         
-        for(int i=0; i<length; i++){
-            int random = rand() % 3;
+//         for(int i=0; i<length; i++){
+//             int random = rand() % 3;
             
-            if(random == 0)
-                hashCode += ('0' + rand() % 10);
-            else if(random == 1)
-                hashCode += ('a' + rand() % 26);
-            else
-                hashCode += ('A' + rand() % 26);
-        }
+//             if(random == 0)
+//                 hashCode += ('0' + rand() % 10);
+//             else if(random == 1)
+//                 hashCode += ('a' + rand() % 26);
+//             else
+//                 hashCode += ('A' + rand() % 26);
+//         }
         
-        if(mp.find(hashCode) !=mp.end())
-            return hashing(longUrl);
-        else{
-            mp[hashCode] = longUrl;
-            return hashCode;
-        }
-    }
-    // Encodes a URL to a shortened URL.
-    string encode(string longUrl) {
-        string tinyUrl = "http://tinyurl.com/";
-        string hashCode = hashing(longUrl);
-        return tinyUrl + hashCode;
-    }
+//         if(mp.find(hashCode) !=mp.end())
+//             return hashing(longUrl);
+//         else{
+//             mp[hashCode] = longUrl;
+//             return hashCode;
+//         }
+//     }
+//     // Encodes a URL to a shortened URL.
+//     string encode(string longUrl) {
+//         string tinyUrl = "http://tinyurl.com/";
+//         string hashCode = hashing(longUrl);
+//         return tinyUrl + hashCode;
+//     }
     
-    // Decodes a shortened URL to its original URL.
-    string decode(string shortUrl) {
-        string hashCode = shortUrl.substr(shortUrl.size() - length, length);
-        return mp[hashCode];
-    }
+//     // Decodes a shortened URL to its original URL.
+//     string decode(string shortUrl) {
+//         string hashCode = shortUrl.substr(shortUrl.size() - length, length);
+//         return mp[hashCode];
+//     }
 };
 
 // Your Solution object will be instantiated and called as such:
