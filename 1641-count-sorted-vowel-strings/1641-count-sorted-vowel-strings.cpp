9334fd413eee5411affb2767d45c95eabebc224f
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-05-11 23:37:59 +0530

    Time: 5 ms (24.57%), Space: 5.9 MB (85.48%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,14 +1,34 @@
+// class Solution {
+// public:
+//     int countVowelStrings(int n) {
+//         int ans = 0;
+//         for (int j=1; j<=(n+1); j++) {
+//             int sum = 0;
+//             for (int i=1; i<=j; i++) {
+//                 sum += i;
+//                 ans += sum;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int countVowelStrings(int n) {
-        int ans = 0;
-        for (int j=1; j<=(n+1); j++) {
-            int sum = 0;
-            for (int i=1; i<=j; i++) {
-                sum += i;
-                ans += sum;
-            }
+        int a = 1;
+        int e = 1;
+        int i = 1;
+        int o = 1;
+        int u = 1;
+        
+        while (--n){
+            e += a;
+            i += e;
+            o += i;
+            u += o;         
         }
-        return ans;
+        return a + e + i + o + u;
     }
 };
