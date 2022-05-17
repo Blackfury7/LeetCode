6939516f192e958b7822ba38f8dd267d7bf25891
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-05-17 23:14:24 +0530

    Time: 570 ms (81.54%), Space: 164 MB (58.30%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -8,11 +8,36 @@
  * };
  */
 
+// class Solution {
+// public:
+//     TreeNode* getTargetCopy(TreeNode* orig, TreeNode* clone, TreeNode* targ) {
+//         return orig == nullptr ? nullptr :
+//             orig == targ ? clone :
+//                 getTargetCopy(orig->left, clone->left, targ) ?: getTargetCopy(orig->right, clone->right, targ);
+//     }
+// };
+
+
+//******************************************************************************************************
+
 class Solution {
+    void dfs(TreeNode* og, TreeNode* copy, TreeNode* target, TreeNode*& ans) {
+        if(!og) return;
+        
+        if(og == target) {
+            ans = copy;
+        }
+        
+        dfs(og->left, copy->left, target, ans);
+        dfs(og->right, copy->right, target, ans);
+        
+        return;
+    }
 public:
-    TreeNode* getTargetCopy(TreeNode* orig, TreeNode* clone, TreeNode* targ) {
-        return orig == nullptr ? nullptr :
-            orig == targ ? clone :
-                getTargetCopy(orig->left, clone->left, targ) ?: getTargetCopy(orig->right, clone->right, targ);
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        
+        TreeNode* ans = nullptr;
+        dfs(original, cloned, target, ans);
+        return ans;
     }
 };
