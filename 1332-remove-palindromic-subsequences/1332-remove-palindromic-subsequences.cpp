3ee326ebe199b619c2a16f53b5dd479747321e76
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-06-08 23:46:50 +0530

    Time: 5 ms (17.09%), Space: 6.1 MB (97.70%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-        return 2 - (s == string(s.rbegin(), s.rend())) - s.empty();
+        // return 2 - (s == string(s.rbegin(), s.rend())) - s.empty();
+        return 2 - equal(s.begin(), s.end(), s.rbegin()) - s.empty();
     }
 };
