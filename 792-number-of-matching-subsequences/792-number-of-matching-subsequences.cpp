c6417f94642558f03c5f41155defad39a7b2b462
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-07-21 01:17:12 +0530

    Time: 326 ms (59.96%), Space: 48.1 MB (62.91%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
-   int numMatchingSubseq(string S, vector<string>& words) {
-        vector<const char*> waiting[128];
-        for (auto &w : words)
-            waiting[w[0]].push_back(w.c_str());
-        for (char c : S) {
-            auto advance = waiting[c];
-            waiting[c].clear();
-            for (auto it : advance)
-                waiting[*++it].push_back(it);
+   // int numMatchingSubseq(string S, vector<string>& words) {
+   //      vector<const char*> waiting[128];
+   //      for (auto &w : words)
+   //          waiting[w[0]].push_back(w.c_str());
+   //      for (char c : S) {
+   //          auto advance = waiting[c];
+   //          waiting[c].clear();
+   //          for (auto it : advance)
+   //              waiting[*++it].push_back(it);
+   //      }
+   //      return waiting[0].size();
+   //  }
+    //*********************************************************************
+    
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int ans = 0;
+        vector<vector<int>> mappings(26);
+        for(int i = 0; i< size(s); i++) mappings[s[i] - 'a'].push_back(i);
+        for(auto &word: words) {
+            bool found = true;
+            // i = index in word | prev = index in s match for word[i - 1]
+            for(int i = 0, prev = -1; found && i < size(word); i++) {
+                auto& v = mappings[word[i] - 'a'];
+                auto it = upper_bound(begin(v), end(v), prev); // check if current character exists in s with index > prev
+                if(it == end(v)) found = false;
+                else prev = *it;
+            }
+            ans += found;
         }
-        return waiting[0].size();
+        return ans;
+        
     }
 };
