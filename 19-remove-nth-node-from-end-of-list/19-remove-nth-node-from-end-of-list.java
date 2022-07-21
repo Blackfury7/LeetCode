8e72727a18a5057a448d57083efbc52553a895b0
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-07-22 02:32:16 +0530

    Time: 1 ms (70.27%), Space: 43.1 MB (7.66%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -8,59 +8,25 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode removeNthFromEnd(ListNode head, int n) {
-        
-//     }
-// }
-
-
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int position) {
-        ListNode curr = head; int n=0;
-        while(curr!=null)
-        {
+        ListNode temp1 = head; int n=0;
+        while(temp1!=null){
             n++;
-            curr = curr.next;
+            temp1 = temp1.next;
         }
-        System.out.println(n);
-        System.out.println(n-position);
-        //if there is only one node or less
-        if(n<2)return null;
-        
-           // if (head == null)
-           //  return head;
-        ListNode temp = head;
-      // if (position == 0)
-      //   {
-      //       head = temp.next;   
-      //       return head;
-      //   }
-        int i=1; ListNode temp1=head;
+        int i=1; 
         // if position is head
         if(n-position == 0){
             head=head.next;
             return head;
         }
-         
-    while(i<n-position){
-            i++;
-            temp1=temp1.next;   
+        temp1=head;
+        while(i<n-position){
+                i++;
+                temp1=temp1.next;   
             }
-        
-        // if position is end node
-        // if(position == 0){
-        //     temp1.next=null;
-        //     return head;
-        // }
-        
-        // if position is in middle
         temp1.next = temp1.next.next;
-           
-        // if (temp == null || temp.next == null)
-        //     return null;
-//         ListNode next = temp.next.next;
-//         temp.next = next.next;
         return head;
     }
 }
