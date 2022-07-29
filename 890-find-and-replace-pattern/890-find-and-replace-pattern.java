b8771ccc0b9a1dcc143e1e9c744f641116ac4dcd
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-07-29 20:22:13 +0530

    Time: 3 ms (54.32%), Space: 43.2 MB (41.36%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        int[] p = F(pattern);
+        List<String> res = new ArrayList<String>();
+        for (String w : words)
+            if (Arrays.equals(F(w), p)) res.add(w);
+        return res;
+    }
+
+    public int[] F(String w) {
+        HashMap<Character, Integer> m = new HashMap<>();
+        int n = w.length();
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            m.putIfAbsent(w.charAt(i), m.size());
+            res[i] = m.get(w.charAt(i));
+        }
+        return res;
+    }
+}
