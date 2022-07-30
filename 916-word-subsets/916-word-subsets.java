1081379256b4c3ad50d2f179303c618165df0c8e
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-07-30 18:15:55 +0530

    Time: 34 ms (63.82%), Space: 89.9 MB (44.31%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        int[] count = new int[26], tmp;
+        int i;
+        for(String b : B) {
+            tmp = counter(b);
+            for( i = 0; i < 26; i++)
+                count[i] = Math.max(count[i], tmp[i]);
+        }
+        List<String> ans = new ArrayList<>();
+        for(String a : A) {
+            tmp = counter(a);
+            for(i = 0; i < 26; i++)
+                if(tmp[i] < count[i])
+                    break;
+            if(i == 26) ans.add(a);
+            
+        }
+        return ans;
+    }
+    
+    int[] counter(String word) {
+        int[] count = new int[26];
+        for(char c : word.toCharArray()) count[c - 'a']++;
+        return count;
+    }
+}
