16eb41918f68ce7a6866092345e53d3ce0ab8073
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-08-29 03:08:08 +0530

    Time: 19 ms (60.98%), Space: 10 MB (76.25%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+     // Approach 
+     //We will use boundary DFS to solve this problem
+        
+      // Let's analyze when an 'O' cannot be flipped,
+      // if it has atleast one 'O' in it's adjacent,
+    // AND ultimately this chain of adjacent 'O's is connected to some 'O' which lies on boundary of board
+        
+      //consider these two cases for clarity :
+      /*
+        O's won't be flipped          O's will be flipped
+        [X O X X X]                   [X X X X X]     
+        [X O O O X]                   [X O O O X]
+        [X O X X X]                   [X O X X X] 
+        [X X X X X]                   [X X X X X]
+      
+      So we can conclude 
+      if a chain of adjacent O's is connected some O on boundary then they cannot be flipped
+      
+      */
+        
+      //Steps to Solve :
+      //1. Move over the boundary of board, and find O's 
+      //2. Every time we find an O, perform DFS from it's position
+      //3. In DFS convert all 'O' to '#'     
+    // (why?? so that we can differentiate which 'O' can be flipped and which cannot be)   
+      //4. After all DFSs have been performed, board contains three elements,#,O and X
+      //5. 'O' are left over elements which are not connected to any boundary O, so flip them to 'X'
+      //6. '#' are elements which cannot be flipped to 'X', so flip them back to 'O'
+    
+    
+    
     int n, m;
     bool isValid(int x, int y, vector<vector<char>>&v) {
         return (x >=0 && x<m && y>=0 && y <n && v[x][y] =='O');
