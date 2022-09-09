706f2a29e8230941b93ca096d4809a2fe3236feb
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-09-09 18:27:41 +0530

    Time: 974 ms (62.64%), Space: 125.5 MB (42.05%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
+    // int numberOfWeakCharacters(vector<vector<int>>& ps) {
+    //     int res = 0, max_def = INT_MIN;    
+    //     sort(begin(ps), end(ps), [](const auto &a, const auto &b) { 
+    //         return a[0] == b[0] ? a[1] < b[1] : a[0] > b[0]; });
+    //     for (const auto &p : ps) {
+    //         res += p[1] < max_def;
+    //         max_def = max(max_def, p[1]);
+    //     }
+    //     return res;
+    // }
+    
+    
+    
+    //TC: O(N)
     int numberOfWeakCharacters(vector<vector<int>>& ps) {
-        int res = 0, max_def = INT_MIN;    
-        sort(begin(ps), end(ps), [](const auto &a, const auto &b) { 
-            return a[0] == b[0] ? a[1] < b[1] : a[0] > b[0]; });
-        for (const auto &p : ps) {
-            res += p[1] < max_def;
-            max_def = max(max_def, p[1]);
-        }
+        int max_d[100002] = {}, res = 0;
+        for (auto &p : ps)
+            max_d[p[0]] = max(max_d[p[0]], p[1]);
+        for (int i = 100000; i > 0; --i)
+            max_d[i - 1] = max(max_d[i - 1], max_d[i]);
+        for (auto &p : ps)
+            res += max_d[p[0] + 1] > p[1];
         return res;
     }
 };
