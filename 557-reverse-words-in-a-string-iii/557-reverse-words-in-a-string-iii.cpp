19c4da161211621ebbc9342cf16429b5fe688253
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-09-22 21:43:16 +0530

    Time: 15 ms (85.69%), Space: 9.5 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -4,7 +4,7 @@ public:
         for (int i = 0; i < s.length(); i++) {
             if (s[i] != ' ') {   // when i is a non-space
                 int j = i;
-                for (; j < s.length() && s[j] != ' '; j++) { } // move j to the next space
+                for (; j < s.length() && s[j] != ' '; j++) ;
                 reverse(s.begin() + i, s.begin() + j);
                 i = j - 1;
             }
