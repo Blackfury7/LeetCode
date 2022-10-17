ed252195e972b7934ee69491bf18634ded425cf7
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-10-17 20:42:35 +0530

    Time: 3 ms (80.43%), Space: 6.4 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool checkIfPangram(string sen) {
+        // 
         // unordered_set<char>s;
         // for(auto &i : sen) s.insert(i);
         // return s.size() == 26;
-        return set<char>(sen.begin(), sen.end()).size() == 26;
+        
+        // one liner code
+        // return set<char>(sen.begin(), sen.end()).size() == 26;
+        
+        // 3rd logic
+        int seen = 0;
+        for( char c : sen) {
+            int ci = c - 'a' ;
+            seen = seen | (1 << ci);
+        }
+        return seen == ((1 << 26) - 1);
     }
 };
