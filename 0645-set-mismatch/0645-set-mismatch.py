72ea156930b597e5941196f84792ba9ffc194ddd
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2022-10-23 17:55:26 +0530

    Time: 390 ms (63.00%), Space: 15.9 MB (41.20%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,4 +1,4 @@
 class Solution:
-    def findErrorNums(self, nums):
+    def findErrorNums(self, nums: List[int]) -> List[int]:
         return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
         
