1466e4aecc76bac448023921d8889c4b9ba9b49a
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2023-01-30 20:33:37 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+       int a[38];
+       a[0] = 0;
+       a[1] = 1;
+       a[2] = 1;
+       for(int i=3; i<38; i++)
+       {
+           a[i] = a[i-1]+a[i-2]+a[i-3];
+       }
+       return a[n];
+    }
+};
