1955e0b8c3f5a38c494ed4aa82690b83a15ba790
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2023-02-02 22:42:58 +0530

    Time: 3 ms (93.87%), Space: 9.7 MB (49.27%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,38 +1,12 @@
 class Solution {
 public:
-     bool Helper(string &w1,string &w2,unordered_map<char,int> &mp)
-     {
-         int n = w1.length();
-         int m = w2.length();
-
-         for(int i=0;i<min(n,m);i++)
-         {
-             if(mp[w1[i]] < mp[w2[i]])
-             {
-                 return true;
-             }
-             else if(mp[w1[i]] > mp[w2[i]]){
-                 return false;
-             }
-         }
-
-         if(m>=n)return true;
-         return false;
-
-     }
-    bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char,int> mp;
-        for(int i=0;i<order.length();i++)
-        {
-            mp[order[i]] = i;
-        }
-        for(int i=0;i<words.size()-1;i++)
-        {
-            if(!Helper(words[i],words[i+1],mp))
-            {
-                return false;
-            }
-        }
-        return true;
+     bool isAlienSorted(vector<string> words, string order) {
+        int mapping[26];
+        for (int i = 0; i < 26; i++)
+            mapping[order[i] - 'a'] = i;
+        for (string &w : words)
+            for (char &c : w)
+                c = mapping[c - 'a'];
+        return is_sorted(words.begin(), words.end());
     }
 };
