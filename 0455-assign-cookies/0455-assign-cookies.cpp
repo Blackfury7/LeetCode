02b495c85281f48ea8522992120e281c21d14dd3
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-01-01 22:47:11 +0530

    Time: 68 ms (5.39%), Space: 17.8 MB (75.56%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int cookiesNums = s.size();
+        if(cookiesNums == 0)  return 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int maxNum = 0;
+        int cookieIndex = cookiesNums - 1;
+        int childIndex = g.size() - 1;
+        while(cookieIndex >= 0 && childIndex >=0){
+            if(s[cookieIndex] >= g[childIndex]){
+                maxNum++;
+                cookieIndex--;
+                childIndex--;
+            }
+            else{
+                childIndex--;
+            }
+        }
+        return maxNum;
+    }
+};
