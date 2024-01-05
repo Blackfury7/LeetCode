0399431ede1891731784c9b207bf9b6afc10a6ab
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-01-05 23:05:04 +0530

    Time: 10 ms (82.11%), Space: 11 MB (46.64%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    // Binary Search approach
+    int lengthOfLIS(vector<int>& nums) {
+    vector<int> res;
+        for(int i=0; i<nums.size(); i++) {
+            auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+            if(it==res.end()) res.push_back(nums[i]);
+            else *it = nums[i];
+        }
+        return res.size();
+    }
+    
+    
+    //***********************************************************************
+    
+    
+    // Dp solution 
+//     int lis(vector<int>&nums, int i, vector<int>&dp) {
+//         if(dp[i] != -1) return dp[i];
+//         int ans = 1;
+//         for(int j = 0; j < i; j++) {
+//             if(nums[i] > nums[j])
+//                 ans = max(ans, lis(nums, j, dp) + 1);
+//         }
+//         return dp[i] = ans;
+//     }
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         int ans = 0;
+//         vector<int>dp(n,-1);
+        
+//         for(int i = 0; i < n; i++) 
+//             ans = max(ans, lis(nums, i, dp));
+        
+//         return ans;
+//     }
+};
