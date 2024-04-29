a0c307631534610af5a56e9b487dd02b26925606
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-04-29 16:12:00 +0530

    Time: 15 ms (36.83%), Space: 22.2 MB (15.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<int>dp(amount+1, 0);
-        dp[0] = 1;
-        for(int i=0; i<n; i++){
-            for(int j=1; j<=amount; j++){
-                if((j-coins[i])>=0){
-                    dp[j] = dp[j] + dp[j-coins[i]];
-                }
-            }
+    
+    int dp[310][10010];
+
+    int func2(int ind, int amount, vector<int>& coins) {
+        if(amount == 0) return 1;
+        if(ind < 0) return 0;
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+
+        int ways = 0;
+        for(int i = 0; i <=ind; i++) {
+            if(amount >= coins[i])
+                ways += func2(i, amount - coins[i], coins);
         }
-        return dp[amount];
+        return dp[ind][amount] = ways;
     }
+    
+    int change(int amount, vector<int>& coins) {
+        memset(dp, -1, sizeof(dp));
+        return func2(coins.size()-1, amount, coins);
+    }
+    
+    
+    
+ // *******************************************************************************   
+    
+    // int change(int amount, vector<int>& coins) {
+    //     int n = coins.size();
+    //     vector<int>dp(amount+1, 0);
+    //     dp[0] = 1;
+    //     for(int i=0; i<n; i++){
+    //         for(int j=1; j<=amount; j++){
+    //             if((j-coins[i])>=0){
+    //                 dp[j] = dp[j] + dp[j-coins[i]];
+    //             }
+    //         }
+    //     }
+    //     return dp[amount];
+    // }
 };
