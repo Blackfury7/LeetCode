18291909e265633dae6018c221e77840c90a0200
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-04-29 14:46:44 +0530

    Time: 112 ms (35.44%), Space: 12.7 MB (80.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     
-    int lis(int i, vector<int>&a, vector<int>&dp) {
+    int dp[2510];
+    
+    int lis(int i, vector<int>&a) {
         if(dp[i] != -1) return dp[i];
         int ans = 1;
         for(int j=0; j < i; j++) {
             if(a[i] > a[j])
-                ans = max(ans, lis(j, a, dp) + 1);
+                ans = max(ans, lis(j, a) + 1);
         }
         return dp[i] = ans;
     }
     int lengthOfLIS(vector<int>& a) {
         int n = a.size();
-        vector<int> dp(n,-1);
+        memset(dp, -1, sizeof(dp));
         int ans = 0;
         
         for(int i = 0; i < n; i++) {
-            ans = max(ans, lis(i, a, dp));
+            ans = max(ans, lis(i, a));
         }
         return ans;
     }
