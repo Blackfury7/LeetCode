8eb64becd988e05c35917a63c3b55f4e5376ce96
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-05-01 23:39:44 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (65.62%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        auto p = word.find(ch);
+        if (p != string::npos)
+            reverse(begin(word), begin(word) + p + 1);
+        return word;
+    }
+};
