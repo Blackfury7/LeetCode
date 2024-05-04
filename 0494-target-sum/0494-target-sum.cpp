9248f905b767e97597158708f29ddea50c3bf1e9
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-05-04 08:15:30 +0530

    Time: 3 ms (93.70%), Space: 11 MB (65.27%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,8 @@
                                                                                             class Solution {
 public:
-    int dp[21][4005];                                                                                        
+    int dp[21][4005];   // since -1000<= target<=1000 and sum(nums) = 1000
+    // so range of target + sum(nums) is [-2000, 2000]
+                                                                                                
     int func(int ind, int target, vector<int>& nums) {
         if(ind < 0){
             if(target == 0) return 1;
@@ -8,7 +10,7 @@ public:
         }
         if(dp[ind][target + 2001] != -1) return dp[ind][target + 2001];
         
-        return dp[ind][target+2001] = func(ind-1, target - nums[ind], nums) + func(ind-1, target + nums[ind], nums);
+        return dp[ind][target + 2001] = func(ind-1, target - nums[ind], nums) + func(ind-1, target + nums[ind], nums);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         memset(dp, -1, sizeof(dp));
