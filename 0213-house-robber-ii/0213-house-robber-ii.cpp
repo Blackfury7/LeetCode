981e4a7a0c8aa60a7cd221a37fcf9a48848728c5
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-05-04 05:34:19 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (64.05%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int n;
     int dp[105][2][2];
-    int func(int ind, int isLast, int isFirst, vector<int>& v ){
+    int func(int ind, int isLastChoosen, int isFirstChoosen, vector<int>& v ){
         if(ind <0) return 0;
-        if(dp[ind][isLast][isFirst] != -1) return dp[ind][isLast][isFirst];
+        if(dp[ind][isLastChoosen][isFirstChoosen] != -1) 
+            return dp[ind][isLastChoosen][isFirstChoosen];
         
         int ans = 0;
-        ans = func(ind - 1, 1, isFirst, v);
-        if(isLast)  {
-            if(ind == n-1) isFirst = 0;
-            if((ind != 0) or (ind == 0 and isFirst))
-            ans = max(ans, func(ind -1, 0, isFirst, v) + v[ind]);
+        ans = func(ind - 1, 0, isFirstChoosen, v);
+        if(!isLastChoosen)  {
+            if(ind == n-1) isFirstChoosen = 1;
+            if((ind != 0) or (ind == 0 and !isFirstChoosen))
+            ans = max(ans, func(ind -1, 1, isFirstChoosen, v) + v[ind]);
         }
-        return dp[ind][isLast][isFirst] = ans; 
+        return dp[ind][isLastChoosen][isFirstChoosen] = ans; 
     }
     
     int rob(vector<int>& nums) {
         
         n = nums.size();
         if(n==1) return nums[0];
-        // memset(dp, -1, sizeof(dp));
         for(int i = 0; i <= n; i++) {
             for(int j = 0; j < 2; j++){
                 dp[i][j][0] = dp[i][j][1] =-1;
             }
         }
-        return func(n-1, 1, 1, nums);
+        return func(n-1, 0, 0, nums);
     }
 };
