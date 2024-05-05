857f0d85e10aeb119cb904df5fd81ca66362b462
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-05-06 04:30:16 +0530

    Time: 18 ms (35.01%), Space: 11.7 MB (26.56%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int N;
     int dp[1001][1001];
+    
     int func(int ind, int copied) {
         if(ind == N) return 0;
         if(ind > N) return 1000;
         if(dp[ind][copied] != -1) return dp[ind][copied];
         
         int ans = 1e3;
-        if(ind != copied)
         // copy
-         ans = min(ans, func(ind, ind) + 1);
+        if(ind != copied)
+            ans = min(ans, func(ind, ind) + 1);
         
         // paste
         if(copied && ind + copied <= N)
-        ans = min(ans, func(ind + copied, copied) + 1);
+            ans = min(ans, func(ind + copied, copied) + 1);
         
         return dp[ind][copied] = ans;
-        
     }
+    
     int minSteps(int n) {
         if(n==1) return 0;
         N = n;
