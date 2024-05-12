50d7116f92d1e5617df23a54164a7301f5d7d8a8
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-05-12 09:56:09 +0530

    Time: 6 ms (60.00%), Space: 8.4 MB (100.00%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    
+    
     int findPermutationDifference(string s, string t) {
-        map<char, int>m;
         int n = s.size();
-        for(int i = 0; i < n; i++){
-            m[s[i]] = i;
-        }
         int ans = 0;
-        for(int i = 0 ; i < n; i++) {
-            ans += abs(m[t[i]] - i);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(s[i] == t[j]) ans += abs(i - j);
+            }
         }
         return ans;
     }
+    
+    // int findPermutationDifference(string s, string t) {
+    //     map<char, int>m;
+    //     int n = s.size();
+    //     for(int i = 0; i < n; i++){
+    //         m[s[i]] = i;
+    //     }
+    //     int ans = 0;
+    //     for(int i = 0 ; i < n; i++) {
+    //         ans += abs(m[t[i]] - i);
+    //     }
+    //     return ans;
+    // }
 };
