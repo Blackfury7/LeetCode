c5369e1c9ccb7c2c7ad46ef1e6de1e6294fca6e4
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-05-12 09:54:26 +0530

    Time: 154 ms (50.00%), Space: 145.3 MB (100.00%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
+    
     int maximumEnergy(vector<int>& e, int k) {
-        vector<int> v(k);        
-        int n = e.size();        
-        for(int i = 0; i < n; i++) {
-            if(v[i%k] < 0){
-                v[i%k] = 0;
-            }            
-            v[i%k] += e[i];
+        vector<int> res(k, 0);
+        int n = e.size();
+        int ans = -(1<<30);
+        for(int i = n - 1; i >= 0; i--){
+            res[i % k] += e[i];
+            ans = max(ans, res[i % k]);
         }
-        return *max_element(v.begin(), v.end());        
+        return ans;
     }
+    
+    // int maximumEnergy(vector<int>& e, int k) {
+    //     vector<int> v(k);        
+    //     int n = e.size();        
+    //     for(int i = 0; i < n; i++) {
+    //         if(v[i%k] < 0){
+    //             v[i%k] = 0;
+    //         }            
+    //         v[i%k] += e[i];
+    //     }
+    //     return *max_element(v.begin(), v.end());        
+    // }
 };
