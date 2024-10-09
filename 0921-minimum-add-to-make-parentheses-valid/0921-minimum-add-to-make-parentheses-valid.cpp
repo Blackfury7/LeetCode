59d2438f67e99c729653c4f8e8e909f2cc812857
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-10-09 23:24:18 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minAddToMakeValid(string S) {
+        int left = 0, right = 0;
+        for (char c : S)
+            if (c == '(')
+                right++;
+            else if (right > 0)
+                right--;
+            else
+                left++;
+        return left + right;
+    }
+};
