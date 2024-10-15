03a863dcffee80ff3ec9f43204bf7c1743685b55
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-10-15 23:52:16 +0530

    Time: 31 ms (73.47%), Space: 15.6 MB (45.68%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long res = 0;
+        for (int i = 0, cnt = 0; i < s.size(); ++i) {
+            if (s[i] == '1')
+                ++cnt;
+            else
+                res += cnt;
+        }
+        return res;
+    }
+};
