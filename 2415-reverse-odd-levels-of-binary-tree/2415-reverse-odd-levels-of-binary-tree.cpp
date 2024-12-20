259c9d5458e487c93927d01b19dd546788549a8a
Divyanshu Sharma <divyanshusharmabb07@gmail.com>
2024-12-20 23:49:10 +0530

    Time: 1 ms (56.96%), Space: 83.1 MB (26.01%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -10,24 +10,65 @@
  * };
  */
 class Solution {
-public:
-        // preorder traversal
-        void dfs (TreeNode* a, TreeNode* b, int level) {
-            if(!a || !b) return;
-            if(level & 1) {
-                cout<<level<<endl;
-                swap(a->val, b->val);
-            }
-            dfs(a->left, b->right, level+1);
-            dfs(a->right, b->left, level+1); 
-        }
+    public:
+    
+    
+    // ************************************************************************************************
+    // preorder traversal
+//         void dfs (TreeNode* a, TreeNode* b, int level) {
+//             if(!a || !b) return;
+//             if(level & 1) {
+//                 cout<<level<<endl;
+//                 swap(a->val, b->val);
+//             }
+//             dfs(a->left, b->right, level+1);
+//             dfs(a->right, b->left, level+1); 
+//         }
 
-    TreeNode* reverseOddLevels(TreeNode* root) {
-        TreeNode* ans;
+//     TreeNode* reverseOddLevels(TreeNode* root) {
+//         TreeNode* ans;
         
-        // preorder traversal
-        dfs(root->left, root->right, 1) ;
+//         // preorder traversal
+//         dfs(root->left, root->right, 1) ;
         
+//         return root;
+//     }
+    
+    // ************************************************************************************************
+    TreeNode* reverseOddLevels(TreeNode* root) 
+    {
+        if(!root) return root;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> ans;
+        int level=0;
+        
+        while(!q.empty())
+        {
+            int s = q.size();
+            vector<int> curr;
+            for(int i=0;i<s;i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if(level%2) node->val = ans[s-i-1];
+                
+                if(node->left) 
+                {
+                    q.push(node->left);
+                    curr.push_back(node->left->val);
+                }
+                if(node->right) 
+                {
+                    q.push(node->right);
+                    curr.push_back(node->right->val);
+                }
+            }
+            ans = curr;
+            level++;
+        }
         return root;
     }
 };
